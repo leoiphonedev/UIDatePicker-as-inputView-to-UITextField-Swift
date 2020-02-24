d229f74ef3ed22331baac1a357e50a0dff830236
Aman Aggarwal <aman@clickapps.co>
2020-02-24 18:52:23 +0530

    Pushing code to repo

--- UIDatePikcer-With-UITextField/ViewController.swift
+++ UIDatePikcer-With-UITextField/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtDate: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.txtDate.setDatePickerAsInputViewFor(target: self, selector: #selector(dateSelected))
+    }
+    
+    @objc func dateSelected() {
+        if let datePicker = self.txtDate.inputView as? UIDatePicker {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateStyle = .medium
+            self.txtDate.text = dateFormatter.string(from: datePicker.date)
+        }
+        self.txtDate.resignFirstResponder()
     }
 
 
